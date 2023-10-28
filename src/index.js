2d5ea83f6afbd3f54dcdb05159dcc67efbb50a1c
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-10-28 21:52:05 +0900

    feat: 기존 채널의 권한 변경 기능 구현 (#11)
    * feat: 기존 채널의 권한 변경 기능 구현
    생성자에게만 채널 관리 권한 부여
    
    Co-authored-by: jinyoung234 <sm099@naver.com>
    Co-authored-by: SWARVY <swarvy0826@naver.com>
    Co-authored-by: cobocho <rlaalsrb1111@naver.com>
    
    * fix: 토큰 및 ID를  evn 변수로 변경
    
    * fix: 사용되지 않는 console 삭제
    
    * fix: 앱 라이센스 env 변수로 변경
    
    ---------
    
    Co-authored-by: jinyoung234 <sm099@naver.com>
    Co-authored-by: SWARVY <swarvy0826@naver.com>
    Co-authored-by: cobocho <rlaalsrb1111@naver.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'dotenv/config';
 
 const commands = [
   {
@@ -27,11 +25,56 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  try {
+    // '스스로 만들기' 카테고리의 ID를 확인합니다.
+    const guild = client.guilds.cache.get(process.env.APP_ID); // 해당 서버의 ID를 넣어야 합니다.
+    const category = guild.channels.cache.find((ch) => ch.name === '스스로 만들기');
+    if (!category) {
+      console.error("Category '스스로 만들기' not found.");
+      return;
+    }
+
+    // '스스로 만들기' 카테고리에 속한 채널들을 가져옵니다.
+    const channelsInCategory = guild.channels.cache.filter((ch) => ch.parentId === category.id);
+
+    // 각 채널에 권한을 설정합니다.
+    channelsInCategory.forEach(async (channel) => {
+      try {
+        // 채널을 만든 사용자의 정보를 가져옵니다.
+        const logs = await guild.fetchAuditLogs({
+          limit: 100,
+          type: 10,
+        });
+        const channelLog = logs.entries.find((entry) => entry.target.id === channel.id);
+        if (!channelLog) {
+          // 해당 채널의 생성 로그 항목이 없는 경우
+          return;
+        }
+
+        // 생성자 ID를 가져옵니다.
+        const creatorId = channelLog.executor.id;
+        const creator = await channel.guild.members.fetch(creatorId);
+
+        // 생성자에게만 'MANAGE_CHANNELS' 권한 부여
+        await channel.permissionOverwrites.edit(creator.id, {
+          ManageChannels: true,
+        });
+
+        // @everyone에 대한 'MANAGE_CHANNELS' 권한 제거
+        await channel.permissionOverwrites.edit(guild.roles.everyone, {
+          ManageChannels: false,
+        });
+      } catch (error) {
+        console.error(`Error setting permissions for channel ${channel.name}:`, error);
+      }
+    });
+  } catch (error) {
+    console.error('Error setting permissions for channels in category:', error);
+  }
 });
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-
   if (interaction.commandName === 'developer') {
     await interaction.reply('https://github.com/woowacourse-bot/discord-bot');
   }
@@ -61,6 +104,5 @@ client.on('channelCreate', async (channel) => {
     console.error('Error setting permissions:', error);
   }
 });
-
 // DISCORD 내 BOT TOKEN을 setToken의 인수로 넣어주세요.
 client.login(process.env.DISCORD_TOKEN);
