c15902c3ef004fe0e25c72777b535c8593fb0c1c
ryan-dia <cjfdnjs158@naver.com>
2023-10-30 10:21:05 +0900

    sytle: 불필요한 주석 제거

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ import readyPermissions from './handlers/channel/readyPermissions.js';
 import channelCreatePermissions from './handlers/channel/channelCreatePermissions.js';
 import about from './interactions/command/about.js';
 
-// for Refactor
 const commands = [
   {
     name: 'developer',
     description: 'Discord-bot Github Repository',
   },
 ];
-// refactor
-// DISCORD 내 BOT TOKEN을 setToken의 인수로 넣어주세요.
+
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  // SETTINGS - General Information - APPLICATION ID를 applicationCommands의 인수로 넣어주세요.
   await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
@@ -31,14 +28,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.on('ready', async () => {
   await readyPermissions(client);
 });
-// refactor
+
 client.on('interactionCreate', async (interaction) => {
   await about(interaction);
 });
-// refactor
+
 client.on('channelCreate', async (channel) => {
   await channelCreatePermissions(channel);
 });
 
-// DISCORD 내 BOT TOKEN을 setToken의 인수로 넣어주세요.
 client.login(process.env.DISCORD_TOKEN);
