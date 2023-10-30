129e2096e5ba56cef6bad72a405ce6cddae68a78
ryan-dia <cjfdnjs158@naver.com>
2023-10-30 12:27:53 +0900

    refactor: commands 상수화

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import 'dotenv/config';
 import readyPermissions from './handlers/channel/readyPermissions.js';
 import about from './interactions/command/about.js';
 import channelCreatePermissions from './event/channel/channelCreatePermissions.js';
-
-const commands = [
-  {
-    name: 'developer',
-    description: 'Discord-bot Github Repository',
-  },
-];
+import ABOUT from './constants/commands.js';
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: ABOUT });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
