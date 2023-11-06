9f1933432301448aff6a29814a22b4fd2aeff62d
cobocho <rlaalsrb1111@naver.com>
2023-11-06 21:31:34 +0900

    refactor: `!` prefix 추출 함수 분리

--- src/event/message/dice.js
+++ src/event/message/dice.js
@@ -1,13 +1,17 @@
 import { MESSAGE_PREFIX } from '../../constants/config.js';
 import { DICE_COMMAND, DICE_NUMBER_RANGE } from '../../constants/dice.js';
 
+const createCommand = (message, prefix) => {
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  return args.shift().toLowerCase();
+};
+
 const dice = (message) => {
   // 메시지가 봇의 메시지이거나 접두사를 가지고 있지 않으면 무시
   if (message.author.bot || !message.content.startsWith(MESSAGE_PREFIX)) return;
 
   // 명령어를 추출하고 공백을 기준으로 나눔
-  const args = message.content.slice(MESSAGE_PREFIX.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+  const command = createCommand(message, MESSAGE_PREFIX);
 
   // 주사위 명령어를 처리
   if (command === DICE_COMMAND) {
