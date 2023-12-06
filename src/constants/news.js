548dc7ad9896ea51cf24db1862f1a418ae6b7a30
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2023-12-06 23:55:25 +0900

    feat: 긱 뉴스 커맨드 기능 구현 (#42)
    * feat : 긱 뉴스 커맨드 기능 구현 
    
    Co-authored-by: SWARVY <swarvy0826@naver.com>
    Co-authored-by: ryan-dia <cjfdnjs158@naver.com>

--- src/constants/news.js
+++ src/constants/news.js
@@ -9,3 +9,14 @@ export const NEWS_THUMBNAIL =
   'https://github.com/woowacourse-bot/discord-bot/assets/76567238/b1a39f10-358a-4007-952c-022738100636';
 
 export const NEWS_TITLE = 'Google 뉴스 - 과학/기술';
+
+export const GEEK_NEWS_COMMAND = '긱뉴스';
+
+export const GEEK_NEWS_AMOUNT = 7;
+
+export const GEEK_NEWS_URL = 'https://feeds.feedburner.com/geeknews-feed';
+
+export const GEEK_NEWS_THUMBNAIL =
+  'https://yt3.googleusercontent.com/ytc/APkrFKZFy5EFhTF2gvqid90gTicknbIO5zXD4gHo09k6=s900-c-k-c0x00ffffff-no-rj';
+
+export const GEEK_NEWS_TITLE = 'Geek News';
